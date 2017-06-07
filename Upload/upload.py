3f13d28816d4ac760c474126df6b3ed15be3bd01
robertburrowes <r.burrowes@auckland.ac.nz>
2017-06-07 15:54:37 +1200

    Markus Python HR upload script.
    Added import of Token from conf/figshare_hr_key.json

---
+++ Upload/upload.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json
+
+FILE_NAME = '../hr_feed_2017-06-06.xml'
+API_URL = 'https://api.figshare.com/v2/institution/hrfeed/upload'
+KEY_FILE = '../conf/figshare_hr_key.json'
+
+with open(KEY_FILE) as json_file:
+    json_data = json.load(json_file)
+
+TOKEN=json_data['hr_figshare_token']
+
+def main():
+    headers = {"Authorization": "token " + TOKEN}
+    with open(FILE_NAME, 'rb') as fin:
+        files = {'hrfeed': (FILE_NAME, fin)}
+        resp = requests.post(API_URL, files=files, headers=headers)
+
+        print(resp.content)
+
+        resp.raise_for_status()
+
+if __name__ == '__main__':
+    main()
