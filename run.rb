650124f962bf07bfd664b904f6990181577b4e26
robertburrowes <r.burrowes@auckland.ac.nz>
2017-06-12 13:22:21 +1200

    tidying

--- run.rb
+++ run.rb
@@ -7,23 +7,26 @@ puts "Started run at #{Time.now}"
 
 @script_dir = File.dirname(__FILE__)
 init #Connect to LDAP, and read the conf files.
+
 user_attributes = {} #We build up all the XML attributes for a user in this hash.
 users_groups = {}    #We fetch user faculty membership, by upi, in this hash.
 get_phd_groups(ldap: @ldap, users_groups: users_groups)
 get_staff_groups(ldap: @ldap, users_groups: users_groups)
+
 #We need to query the LDAP for each users basic details.
 users_groups.each do |k,v|
   user_attributes[k] = get_user_attributies(ldap: @ldap, upi: k, attributes: {'cn' => :upi, 'sn' => :surname, 'givenname'=>:givenname, 'mail'=>:email, 'employeenumber'=>:uoa_id} )
   #Add in the faculty, which isn't in a users basic LDAP attributes.
   user_attributes[k][:primary_group] = @override_group[k] != nil ? @override_group[k] : (v.length == 1 ? v[0] : '')
 end
+
 #Generate the Figshare HR feed XML file from the collected attributes.
 new_filename = "#{@script_dir}/user_xml_files/figshare_hr_feed_#{Time.now.strftime("%Y-%m-%d")}.xml"
 
 gen_xml(users: user_attributes, filename: new_filename)
 
 #automate next file to upload for python script to consume.
-puts "Creating file #{new_filename}"
+puts "Creating hr_file_to_upload.json for xml file: #{new_filename}"
 File.open("#{@script_dir}/Upload/hr_file_to_upload.json","w") do |fd|
   fd.puts "{\n\"filename\": \"#{new_filename}\"\n}"
 end
