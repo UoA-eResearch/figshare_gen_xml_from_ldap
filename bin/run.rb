75a86db7c26a73a47d58b78e9f6c05dc0f1949ce
robertburrowes <r.burrowes@auckland.ac.nz>
2018-08-28 13:55:30 +1200

    Moved to bin

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
-require_relative 'rlib/init.rb'
-require_relative 'rlib/gen_figshare_xml.rb'
-require_relative 'rlib/get_user_attributes.rb'
+require_relative '../rlib/init.rb'
+require_relative '../rlib/gen_figshare_xml.rb'
+require_relative '../rlib/get_user_attributes.rb'
+
+puts "Started run at #{Time.now}"
+
+@script_dir = File.dirname(__FILE__) + '/..'
+init #Connect to LDAP, and read the conf files.
 
 #Ensure we have the upload user in the feed
 FIGSHARE_HR = {
@@ -13,11 +18,6 @@ FIGSHARE_HR = {
   :uoa_id => ''
 }
 
-puts "Started run at #{Time.now}"
-
-@script_dir = File.dirname(__FILE__)
-init #Connect to LDAP, and read the conf files.
-
 user_attributes = {} #We build up all the XML attributes for a user in this hash.
 users_groups = {}    #We fetch user faculty membership, by upi, in this hash.
 get_phd_groups(ldap: @ldap, users_groups: users_groups)
@@ -28,7 +28,9 @@ users_groups.each do |k,v|
   user_attributes[k] = get_user_attributies(ldap: @ldap, upi: k, attributes: {'cn' => :upi, 'sn' => :surname, 'givenname'=>:givenname, 'mail'=>:email, 'employeenumber'=>:uoa_id} )
   #Add in the faculty, which isn't in a users basic LDAP attributes.
   user_attributes[k][:primary_group] = @override_group[k] != nil ? @override_group[k] : (v.length == 1 ? v[0] : '')
+  #user_attributes[k][:force_quota_update] = true
 end
+
 #Add upload user.
 user_attributes[FIGSHARE_HR[:upi]] = FIGSHARE_HR
 
