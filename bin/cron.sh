9a02fa053a5b670af99c5c6229228c377e99cd6f
Rob Burrowes <r.burrowes@auckland.ac.nz>
2018-11-16 13:36:48 +1300

    Wrapped script in call to lockfile

--- bin/cron.sh
+++ bin/cron.sh
@@ -2,6 +2,15 @@
 #Run from cron (crontab -l)
 #1 0 * * * /home/figshare/figshare_gen_xml_from_ldap/cron.sh > /home/figshare/figshare_gen_xml_from_ldap/log/last_run.log 2>&1
 #
+RM="/bin/rm"
+LOCKFILE="/home/figshare/bin/lockfile"
+TMP_DIR="/tmp"
+LOCK_PID_FILE=${TMP_DIR}/figshare_hr_feed.lock
+
+${LOCKFILE} ${LOCK_PID_FILE} $$
+if [ $? != 0 ] ; then  exit 0 ; fi
+
+
 log_date=`date "+%Y-%m-%d"`
 base_dir="/home/figshare/figshare_gen_xml_from_ldap"
 ${base_dir}/bin/run.rb > ${base_dir}/log/run_${log_date}.log 2>&1
@@ -10,3 +19,5 @@ ${base_dir}/bin/upload.py >> ${base_dir}/log/run_${log_date}.log 2>&1
 #
 /usr/bin/find ${base_dir}/log -mtime +30 -exec rm -f {} \;
 /usr/bin/find ${base_dir}/user_xml_files -name figshare_hr_feed\*.xml -mtime +30 -exec rm -f {} \;
+
+${RM} -f ${LOCK_PID_FILE}
