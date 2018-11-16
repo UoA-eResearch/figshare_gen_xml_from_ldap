0dae82d1de16d8a6dd58360483a799a8b56a0d98
Rob Burrowes <r.burrowes@auckland.ac.nz>
2018-11-16 13:38:04 +1300

    Duplicate copy

--- cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Run from cron (crontab -l)
-#1 0 * * * /home/figshare/figshare_gen_xml_from_ldap/cron.sh > /home/figshare/figshare_gen_xml_from_ldap/log/last_run.log 2>&1
-#
-log_date=`date "+%Y-%m-%d"`
-base_dir="/home/figshare/figshare_gen_xml_from_ldap"
-${base_dir}/run.rb > ${base_dir}/log/run_${log_date}.log 2>&1
-#Upload commented out until we get the firewall open.
-#${base_dir}/Upload/upload.py >> ${base_dir}/log/run_${log_date}.log 2>&1
