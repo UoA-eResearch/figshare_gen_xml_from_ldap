47bdb31c22b88b07d299e69baca9a9694578af64
robertburrowes <r.burrowes@auckland.ac.nz>
2023-05-30 10:57:29 +1200

    Preload all user details from Figshare, so an update can compare state, and just do diffs.

--- rlib/figshare_user_record.rb
+++ rlib/figshare_user_record.rb
@@ -1,5 +1,42 @@
 require 'figshare_api_v2'
 
+# Preload current Figshare user records, so we can check quota, and existance of users
+# This is much faster than making > 14,000 individual REST calls. It still takes about 5m.
+# Each Figshare account record is of the form:
+# {  "id":,                       # Figshare's account table index
+#    "user_id":                  # Figshare user table index
+#    "first_name":, "last_name":,
+#    "email":"                   # UoA email address, so either @auckland or @aucklanduni
+#    "active":                   # User's account is active = 1 or inactive = 0
+#                                  We set the quota to 0, rather than setting inactive
+#    "institution_id":           # Constant for all users in the institute
+#    "institution_user_id":      # upi@auckland.ac.nz,
+#    "symplectic_user_id":       # UoA ID number,
+#    "quota":                    # We default to 10,737,418,240, (10G) or 0 for non-current users
+#    "used_quota":
+# }
+# Sets @active_users and @inactive_users, both indexed by Figshares account ID
+# Also sets user indexes @email and @institute_id (i.e. indexed by the upi@auckland.ac.nz)
+#
+# @param active [Integer] Get active (default), inactive (0) or all active: nil?)  users
+def institute_accounts(active: 1)
+  # Globals.
+  @active_users = {}
+  @inactive_users = {}   # We do have a few, from the early days, but then we just set the quota to 0.
+  @email = {}
+  @institute_id = {}     # By UPI@auckland
+
+  @figshare.institutions.accounts(is_active: active) do |a|
+    @email[a['email']] = a
+    @institute_id[a['institution_user_id']] = a
+    if a['active'] == 1
+      @active_users[a['id']] = a
+    else
+      @inactive_users[a['id']] = a
+    end
+  end
+end
+
 # Fetch the Figshare user record, using the UoA identity
 # @param upi [String] UoA identity
 # @yield [Hash] Figshare user record
